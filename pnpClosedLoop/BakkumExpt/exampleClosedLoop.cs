eef6e4f9a452ee259e88b9c31b480a8b0dd90c5d
rzellertownson <rzellertownson@gmail.com>
2010-10-06 00:37:58 +0000

    git-svn-id: https://neurorighter.googlecode.com/svn/branches/NeuroRighter_ClosedLoop_simpler@212 471701b0-e017-11dd-9759-139c9ab14fc6

--- pnpClosedLoop/BakkumExpt/exampleClosedLoop.cs
+++ pnpClosedLoop/BakkumExpt/exampleClosedLoop.cs
@@ -9,7 +9,7 @@ using NeuroRighter;
 
 namespace RileyClosedLoops
 {
-    class BurstSuppression : pnpClosedLoopAbs
+    class exampleClosedLoop : pnpClosedLoopAbs
     {
         //file io
         FileStream fs;
@@ -23,6 +23,9 @@ namespace RileyClosedLoops
         double[] wave;
         int lastStimTime = 0;
         int STIM_SAMP_FREQ = 100000;
+        int SPIKE_SAMP_FREQ = 25000;
+
+
 #region overriden methods
         public override void run()
         {
@@ -37,43 +40,91 @@ namespace RileyClosedLoops
 
 
 
-            //the loop
+            //these aren't directly related to the closed loop guts, but are instead a few objects to 
             count = 0;
-            List<SpikeWaveform> record;
+            List<SpikeWaveform> record;//for holding spikes you have recorded
             string response;
+            
+            //this is a simple stimulus construction method I use for demonstration purposes
             firstStim();
+
+            //SPECIAL STUFF
+
+            //in this experiment, we are going to use a stimulation buffer
+            //this will allow us to look at the spikes that have occured so far, and based on them
+            //create a train of stimuli, which we will then append onto the stimulation buffer
+            //once we tell the stimulation buffer to start, it looks at all the stimuli that have been appended
+            //so far, and starts applying them to the electrodes one by one.
+            //you can let the buffer run out if you want- that just means it will wait for you to add more stimuli
+            //if you tell it to stimulate at a time in the past, your experiment will close with an error.
+
+            //first, put the stimulation buffer together
             CLE.initializeStim();
+
+            //and lets go ahead and start it up- note that since we are starting the stimulation buffer
+            //now, as opposed to at the very beginning of the experiment, there are two clocks running
+            //- the first is for the spikes, which started as soon as the experiment started.  The second is for
+            //the stimulations, which doesn't start until you call the method below.  This second clock is what
+            //is used to figure out when your stimuli are applied- for example, if you decide to stimulate at
+            //time '1.0,' you will stimulate 1 millisecond after the stimBuffStart method gets called.
+            //you can find out the delay between the two clocks using the method CLE.stimOffset()
             CLE.stimBuffStart();
+
+            //the loop
+            //this runs until you hit the 'stop' button in NeuroRighter.  You could include other logic here
+            //if you wanted the experiment to stop for other reasons.
             while (!CLE.isCancelled)
             {
+                //the writeline stuff is just file i/o
                 w.WriteLine(DateTime.Now.TimeOfDay.ToString() + ": appending " + stimToString());
+
+
+                //append stimuli to the stimulation buffer.  As it takes a second for CLE.stimBuffStart to
+                //get going, this code actually puts stimuli on the buffer before the buffer starts stimulating, 
+                //, so we are safe using a stimulus time of '0ms'.
                CLE.appendStim(timevec, channelvec, wavemat);
-               w.WriteLine(DateTime.Now.TimeOfDay.ToString() + ": appended");
+
+              // w.WriteLine(DateTime.Now.TimeOfDay.ToString() + ": appended");
+
+                //this method regenerates the vectors used above- you would include your own code here
                 firstStim();
+                
+                //just a dumb little wait routine.
                 System.Threading.Thread.Sleep(100);
+
+                //SPECIAL STUFF
+                //this method takes all the spikes that have been recorded since the last time you called
+                //this method, and provides them as a list<spikeWaveform>
                 record = CLE.recordClear();
+
+                //here is an example of how you could go through a list like that:
                 response = null;
                 SpikeWaveform current;
                 for (int i = 0; i < record.Count(); i++)
                 {
                     current = record.ElementAt(i);
-                    response += "(" + ((double)current.index)/25000 + ", " + current.channel + ") ";
+                    response += "(" + ((double)current.index) / SPIKE_SAMP_FREQ + ", " + current.channel + ") ";
                     count++;
                 }
                 w.WriteLine(DateTime.Now.TimeOfDay.ToString() + ":" + response);
             
-                
+                //this line demonstrates a couple of nifty little methods:
+                //CLE.StimOffset tells you what time your first stimulation happened, with reference to 
+                //the spikes clock.  
+                //CLE.stimuliInQueue tells you how many stimuli are in the buffer right now
+                //CLE.currentStimTime tells you what the current time is based on the stim buffer clock.  Useful for
+                //making sure you don't get to far ahead of yourself.
                     w.WriteLine(DateTime.Now.TimeOfDay.ToString() + ":stim offset: " + CLE.StimOffset() + " stim in queue: " + CLE.stimuliInQueue()+ ". time ="+CLE.currentStimTime());
             }
 
         }
 
-
+        //this code gets called when you click the 'stop' button
         public override void close()
         {
             try
             {
-            //    CLE.stimBuffStop();
+            //  file i/o stuff
                 w.WriteLine(DateTime.Now.TimeOfDay.ToString() + ":" + count + " spikes recorded");
                 w.WriteLine("closed loop burst suppression experiment stopped at " + DateTime.Now.TimeOfDay.ToString());
                 w.Flush();
@@ -83,13 +134,15 @@ namespace RileyClosedLoops
             }
             catch (Exception e)
             {
-                //close();
+                MessageBox.Show("tried to close, but that didn't work.  hmm." + e.Message);
             }
         }
 
 #endregion
 
         #region helper methods
+
+        //code to update the vectors we are going to append to the stimulation buffer
         public void firstStim()
         {
             wave = biphasicSquarePulse(0.5, 0.4, 0.4);//v,ms,ms

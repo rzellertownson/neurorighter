e27f8ff2500ab95787b91dfd2fd05f110f4e8199
jonnew <jonathan.p.newman@gmail.com>
2011-03-17 03:55:58 +0000

    -added extension methods
    git-svn-id: https://neurorighter.googlecode.com/svn/branches/Stable@265 471701b0-e017-11dd-9759-139c9ab14fc6

---
+++ NeuroRighter_05/ExtentionMethods/ExtMeth01.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExtensionMethods
+{
+    /// <summary>
+    /// Extension Methods for NeuroRighter
+    /// </summary>
+    /// <author> Jon Newman</author>
+    public static class NRExtensionMethods
+    {
+
+        // Method for getting index of max value of 
+        // a generic collection
+        public static int MaxIndex<T>(this IEnumerable<T> sequence)
+            where T : IComparable<T>
+        {
+            int maxIndex = -1;
+            T maxValue = default(T); // Immediately overwritten anyway
+
+            int index = 0;
+            foreach (T value in sequence)
+            {
+                if (value.CompareTo(maxValue) > 0 || maxIndex == -1)
+                {
+                    maxIndex = index;
+                    maxValue = value;
+                }
+                index++;
+            }
+            return maxIndex;
+        }
+
+        public static int MinIndex<T>(this IEnumerable<T> sequence)
+            where T : IComparable<T>
+        {
+            int minIndex = -1;
+            T minValue = default(T); // Immediately overwritten anyway
+
+            int index = 0;
+            foreach (T value in sequence)
+            {
+                if (value.CompareTo(minValue) < 0 || minIndex == -1)
+                {
+                    minIndex = index;
+                    minValue = value;
+                }
+                index++;
+            }
+            return minIndex;
+        }
+    }
+}

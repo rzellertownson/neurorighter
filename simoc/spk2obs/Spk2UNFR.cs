5e09f44d8920f39ecd04a313aae07f8e8d1c6f89
jonnew <jonathan.p.newman@gmail.com>
2011-10-13 21:07:36 +0000

    - Forgot to add changes to simoc
    git-svn-id: https://neurorighter.googlecode.com/svn/trunk@384 471701b0-e017-11dd-9759-139c9ab14fc6

---
+++ simoc/spk2obs/Spk2UNFR.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NeuroRighter.DataTypes;
+using NeuroRighter.Output;
+using NeuroRighter.DatSrv;
+using NeuroRighter.StimSrv;
+
+namespace simoc.spk2obs
+{
+    class Spk2UNFR: Spk2Obs
+    {
+        // Number of detected units
+        private int numberOfDetectedUnits;
+
+        public Spk2UNFR(NRStimSrv stimSrv, NRDataSrv datSrv)
+            : base(stimSrv,datSrv)
+        {
+            numberOfObs = 1;
+        }
+
+        internal void SetNumberOfUnits(int numUnits)
+        {
+            numberOfDetectedUnits = numUnits;
+        }
+
+        internal override void MeasureObservable()
+        {
+            // Estimate the CSDR
+            if (numberOfDetectedUnits > 0)
+                currentObservation = (double)newSpikes.eventBuffer.Where(x => x.unit != 0).Count() / (double)numberOfDetectedUnits; 
+            else
+                currentObservation = 0;
+        }
+    }
+}
---
+++ simoc/targetfunc/CustomTarget2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NeuroRighter.DataTypes;
+using NeuroRighter.Output;
+using NeuroRighter.StimSrv;
+using simoc.UI;
+using simoc.persistantstate;
+
+namespace simoc.targetfunc
+{
+    class CustomTarget2 : TargetFunc
+    {
+        private double[] muliplierTargets = {0,1.5,0,2.0};
+        private double t1 = 1800;
+
+        public CustomTarget2(ControlPanel cp, double DACPollingPeriodSec, ulong numTargetSamplesGenerated, ref NRStimSrv stimSrv)
+            : base(cp, DACPollingPeriodSec, numTargetSamplesGenerated, ref stimSrv)
+        {
+        }
+
+
+        internal override void GetTargetValue(ref double currentTargetValue, PersistentSimocVar simocVariableStorage)
+        {
+            double secondsSinceStart = ((double)(currentOutputSample - simocVariableStorage.SimocStartSample)) / outputSampleRateHz;
+            int currentTargetIndex = (int)Math.Floor(secondsSinceStart / t1);
+
+            if (currentTargetIndex == simocVariableStorage.LastTargetIndex)
+            {
+                if (muliplierTargets.Length > currentTargetIndex)
+                    currentTargetValue = simocVariableStorage.FrozenCumulativeAverageObs * muliplierTargets[currentTargetIndex];
+                else
+                    currentTargetValue = 0.0;
+            }
+            else
+            {
+                simocVariableStorage.FrozenCumulativeAverageObs = simocVariableStorage.CumulativeAverageObs;
+                simocVariableStorage.ResetRunningObsAverage();
+            }
+
+            simocVariableStorage.LastTargetIndex = currentTargetIndex;
+
+        }
+
+
+    }
+}

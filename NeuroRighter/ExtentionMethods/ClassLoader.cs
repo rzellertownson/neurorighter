cbfbf0dc5d3722105ef08612bbdde1f57bc76785
jonnew <jonathan.p.newman@gmail.com>
2012-02-09 04:38:33 +0000

    - Working on classes for dynamic assembly loading
    git-svn-id: https://neurorighter.googlecode.com/svn/trunk@432 471701b0-e017-11dd-9759-139c9ab14fc6

---
+++ NeuroRighter/ExtentionMethods/ClassLoader.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Reflection.Emit;
+
+
+
+namespace ExtensionMethods
+{
+
+    public class ClassLoader
+    {
+
+        public void NewAssembly(String filename)
+        {
+
+            // Create application domain setup information.
+            AppDomainSetup domainSetup = new AppDomainSetup();
+            domainSetup.ApplicationBase = AppDomain.CurrentDomain.BaseDirectory;
+            domainSetup.ApplicationName = "NeuroRighter Closed Loop DLL";
+
+            //Create new domain with domain setup details
+            AppDomain myDomain = AppDomain.CreateDomain("LoaderDomain", null, domainSetup);
+
+            //Create new instance of Loader class, where the dll is loaded.
+            //This is loaded into the new application domian so it can be
+            //unloaded later
+            Loader objGenerator = (Loader)myDomain.CreateInstanceAndUnwrap(Assembly.GetExecutingAssembly().FullName, "NeuroRighter.ExtensionMethods.Loader");
+
+            //Call LoadDll method to load the assembly into the doamin
+
+            objGenerator.LoadDll(filename);
+
+            //Unlload domain
+
+            AppDomain.Unload(myDomain);
+
+        }//end new assembly
+
+    }//end class
+
+}//end namespace
---
+++ NeuroRighter/ExtentionMethods/Loader.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using System.IO;
+
+namespace ExtensionMethods
+{
+
+    class Loader : MarshalByRefObject
+    {
+
+        public void LoadDll(string filename)
+        {
+
+            if (File.Exists(filename))
+            {
+
+                //Loads file into Byte array - prevent file locking
+                byte[] rawAssembly = NRExtensionMethods.LoadFile(filename);
+
+                //algorithmAssembly = Assembly.LoadFrom(filename);
+                Assembly algorithmAssembly = Assembly.Load(rawAssembly);
+
+                foreach (Type TypeAlgorithm in algorithmAssembly.GetExportedTypes())
+                {
+                    if (TypeAlgorithm.IsClass == true)
+                    {
+                        Console.WriteLine("Type is {0}", TypeAlgorithm);
+                        Object ObjAlgorithmInstance = Activator.CreateInstance(TypeAlgorithm);
+
+                        //Invoke method - start() within the dll
+                        MethodInfo methodInfo = ObjAlgorithmInstance.GetType().GetMethod("start");
+                        methodInfo.Invoke(ObjAlgorithmInstance, new object[] { });
+                    }
+                }
+            }
+        }
+    } 
+}

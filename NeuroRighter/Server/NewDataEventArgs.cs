15565df7ab4000e4acaa2bceb289a14925721108
jonnew <jonathan.p.newman@gmail.com>
2012-06-22 15:24:07 +0000

    - Forgot to add ./Server/NewDataEventArgs which is a parameter wrapper for subscribers to the NewData Event.
    git-svn-id: https://neurorighter.googlecode.com/svn/trunk@485 471701b0-e017-11dd-9759-139c9ab14fc6

---
+++ NeuroRighter/Server/NewDataEventArgs.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NeuroRighter.Server
+{
+    /// <summary>
+    /// Event arguements for NewData events within server classes.
+    /// </summary>
+    public class NewDataEventArgs : EventArgs
+    {
+        private ulong firstNeweSample;
+        private ulong lastNeweSample;
+        
+        /// <summary>
+        /// Sample index of the least recent sample in the new data that has been imported into the data server object.
+        /// </summary>
+        public ulong FirstNewSample
+        {
+            internal set
+            {
+                firstNeweSample = value;
+            }
+            get
+            {
+                return this.firstNeweSample;
+            }
+        }
+
+        /// <summary>
+        /// Sample index of the least recent sample in the new data that has been imported into the data server object.
+        /// </summary>
+        public ulong LastNewSample
+        {
+            internal set
+            {
+                lastNeweSample = value;
+            }
+            get
+            {
+                return this.lastNeweSample;
+            }
+        }
+    }
+}

3d56acc700c2b5576a20948aa9cf613c1cf6f8d3
jonnew <jonathan.p.newman@gmail.com>
2012-06-27 21:59:01 +0000

    - Forgot file
    git-svn-id: https://neurorighter.googlecode.com/svn/trunk@498 471701b0-e017-11dd-9759-139c9ab14fc6

---
+++ NeuroRighter/Server/NewRawDataEventArgs.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NeuroRighter.Server
+{
+    public class NewRawDataEventArgs : EventArgs
+    {
+        private ulong firstNewSample;
+        private ulong lastNewSample;
+
+        /// <summary>
+        /// Sample index of the least recent sample in the new data that has been imported into the data server object.
+        /// </summary>
+        public ulong FirstNewSample
+        {
+            internal set
+            {
+                firstNewSample = value;
+            }
+            get
+            {
+                return firstNewSample;
+            }
+        }
+
+        /// <summary>
+        /// Sample index of the least recent sample in the new data that has been imported into the data server object.
+        /// </summary>
+        public ulong LastNewSample
+        {
+            internal set
+            {
+                lastNewSample = value;
+            }
+            get
+            {
+                return lastNewSample;
+            }
+        }
+    }
+}

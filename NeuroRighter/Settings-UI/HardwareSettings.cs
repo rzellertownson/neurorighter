4a0cadb98edab98305db777ad8822ef0ffc6e380
jonnew <jonathan.p.newman@gmail.com>
2012-08-06 18:43:57 +0000

    - Added some error handling code to the hardware settings class in order to route out a setup-specific exception that is mentioned in issue 61.
    git-svn-id: https://neurorighter.googlecode.com/svn/trunk@505 471701b0-e017-11dd-9759-139c9ab14fc6

--- NeuroRighter/Settings-UI/HardwareSettings.cs
+++ NeuroRighter/Settings-UI/HardwareSettings.cs
@@ -47,165 +47,285 @@ namespace NeuroRighter
             HWpersistWindowComponent.XMLFilePath = Properties.Settings.Default.persistWindowPath;
 
             comboBox_analogInputDevice1.Items.AddRange(DaqSystem.Local.Devices);
-            if (comboBox_analogInputDevice1.Items.Count > 0)
+            try
             {
-                int idx = 0;
-                if (Properties.Settings.Default.AnalogInDevice != null)
-                    idx = comboBox_analogInputDevice1.Items.IndexOf(Properties.Settings.Default.AnalogInDevice[0]);
+                if (comboBox_analogInputDevice1.Items.Count > 0)
+                {
+                    int idx = 0;
+                    if (Properties.Settings.Default.AnalogInDevice != null)
+                        idx = comboBox_analogInputDevice1.Items.IndexOf(Properties.Settings.Default.AnalogInDevice[0]);
 
-                if (idx >= 0)
-                    comboBox_analogInputDevice1.SelectedIndex = idx;
-                else
-                    comboBox_analogInputDevice1.SelectedIndex = 0;
+                    if (idx >= 0)
+                        comboBox_analogInputDevice1.SelectedIndex = idx;
+                    else
+                        comboBox_analogInputDevice1.SelectedIndex = 0;
+                }
             }
+            catch
+            {
+                MessageBox.Show("Some default settings may not have loaded properly.");
+            }
+
             comboBox_analogInputDevice2.Items.AddRange(DaqSystem.Local.Devices);
-            if (comboBox_analogInputDevice2.Items.Count > 0)
+            try
             {
-                int idx = -1;
-                if (Properties.Settings.Default.AnalogInDevice != null)
-                    if (Properties.Settings.Default.AnalogInDevice.Count > 1)
-                        idx = comboBox_analogInputDevice2.Items.IndexOf(Properties.Settings.Default.AnalogInDevice[1]);
+                if (comboBox_analogInputDevice2.Items.Count > 0)
+                {
+                    int idx = -1;
+                    if (Properties.Settings.Default.AnalogInDevice != null)
+                        if (Properties.Settings.Default.AnalogInDevice.Count > 1)
+                            idx = comboBox_analogInputDevice2.Items.IndexOf(Properties.Settings.Default.AnalogInDevice[1]);
 
 
-                if (idx >= 0)
-                    comboBox_analogInputDevice2.SelectedIndex = idx;
-                else
-                    comboBox_analogInputDevice2.SelectedIndex = 0;
+                    if (idx >= 0)
+                        comboBox_analogInputDevice2.SelectedIndex = idx;
+                    else
+                        comboBox_analogInputDevice2.SelectedIndex = 0;
+                }
             }
+            catch
+            {
+                MessageBox.Show("Some default settings may not have loaded properly.");
+            }
+
             comboBox_stimulatorDevice.Items.AddRange(DaqSystem.Local.Devices);
-            if (comboBox_stimulatorDevice.Items.Count > 0)
+            try
             {
-                int idx = comboBox_stimulatorDevice.Items.IndexOf(Properties.Settings.Default.StimulatorDevice);
-                if (idx >= 0)
-                    comboBox_stimulatorDevice.SelectedIndex = idx;
-                else
-                    comboBox_stimulatorDevice.SelectedIndex = 0;
+                if (comboBox_stimulatorDevice.Items.Count > 0)
+                {
+                    int idx = comboBox_stimulatorDevice.Items.IndexOf(Properties.Settings.Default.StimulatorDevice);
+                    if (idx >= 0)
+                        comboBox_stimulatorDevice.SelectedIndex = idx;
+                    else
+                        comboBox_stimulatorDevice.SelectedIndex = 0;
+                }
             }
+            catch
+            {
+                MessageBox.Show("Some default settings may not have loaded properly.");
+            }
+
             comboBox_stimInfoDev.Items.AddRange(DaqSystem.Local.Devices);
-            if (comboBox_stimInfoDev.Items.Count > 0)
+            try
             {
-                int idx = comboBox_stimInfoDev.Items.IndexOf(Properties.Settings.Default.StimInfoDevice);
-                if (idx >= 0)
-                    comboBox_stimInfoDev.SelectedIndex = idx;
-                else
-                    comboBox_stimInfoDev.SelectedIndex = 0;
+                if (comboBox_stimInfoDev.Items.Count > 0)
+                {
+                    int idx = comboBox_stimInfoDev.Items.IndexOf(Properties.Settings.Default.StimInfoDevice);
+                    if (idx >= 0)
+                        comboBox_stimInfoDev.SelectedIndex = idx;
+                    else
+                        comboBox_stimInfoDev.SelectedIndex = 0;
+                }
             }
+            catch
+            {
+                MessageBox.Show("Some default settings may not have loaded properly.");
+            }
+
             comboBox_cineplexDevice.Items.AddRange(DaqSystem.Local.Devices);
-            if (comboBox_cineplexDevice.Items.Count > 0)
+            try
             {
-                int idx = comboBox_cineplexDevice.Items.IndexOf(Properties.Settings.Default.CineplexDevice);
-                if (idx >= 0)
-                    comboBox_cineplexDevice.SelectedIndex = idx;
-                else
-                    comboBox_cineplexDevice.SelectedIndex = 0;
+                if (comboBox_cineplexDevice.Items.Count > 0)
+                {
+                    int idx = comboBox_cineplexDevice.Items.IndexOf(Properties.Settings.Default.CineplexDevice);
+                    if (idx >= 0)
+                        comboBox_cineplexDevice.SelectedIndex = idx;
+                    else
+                        comboBox_cineplexDevice.SelectedIndex = 0;
+                }
             }
+            catch
+            {
+                MessageBox.Show("Some default settings may not have loaded properly.");
+            }
+
             comboBox_LFPDevice1.Items.AddRange(DaqSystem.Local.Devices);
-            if (comboBox_LFPDevice1.Items.Count > 0)
+            try
             {
-                int idx = comboBox_LFPDevice1.Items.IndexOf(Properties.Settings.Default.LFPDevice);
-                if (idx >= 0)
-                    comboBox_LFPDevice1.SelectedIndex = idx;
-                else
-                    comboBox_LFPDevice1.SelectedIndex = 0;
+                if (comboBox_LFPDevice1.Items.Count > 0)
+                {
+                    int idx = comboBox_LFPDevice1.Items.IndexOf(Properties.Settings.Default.LFPDevice);
+                    if (idx >= 0)
+                        comboBox_LFPDevice1.SelectedIndex = idx;
+                    else
+                        comboBox_LFPDevice1.SelectedIndex = 0;
+                }
             }
-            comboBox_LFPDevice2.Items.AddRange(DaqSystem.Local.Devices);
-            if (comboBox_LFPDevice2.Items.Count > 0)
+            catch
             {
-                int idx = comboBox_LFPDevice2.Items.IndexOf(Properties.Settings.Default.LFPDevice);
-                if (idx >= 0)
-                    comboBox_LFPDevice2.SelectedIndex = idx;
-                else
-                    comboBox_LFPDevice2.SelectedIndex = 0;
+                MessageBox.Show("Some default settings may not have loaded properly.");
+            }
+
+            comboBox_LFPDevice2.Items.AddRange(DaqSystem.Local.Devices);
+            try
+            {
+                if (comboBox_LFPDevice2.Items.Count > 0)
+                {
+                    int idx = comboBox_LFPDevice2.Items.IndexOf(Properties.Settings.Default.LFPDevice);
+                    if (idx >= 0)
+                        comboBox_LFPDevice2.SelectedIndex = idx;
+                    else
+                        comboBox_LFPDevice2.SelectedIndex = 0;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Some default settings may not have loaded properly.");
             }
 
             string[] serialPortNames = System.IO.Ports.SerialPort.GetPortNames();
             comboBox_progRefSerialPort.Items.AddRange(serialPortNames);
-            if (comboBox_progRefSerialPort.Items.Count > 0)
+            try
             {
-                int idx = comboBox_progRefSerialPort.Items.IndexOf(Properties.Settings.Default.SerialPortDevice);
-                if (idx >= 0)
-                    comboBox_progRefSerialPort.SelectedIndex = idx;
-                else
-                    comboBox_progRefSerialPort.SelectedIndex = 0;
+                if (comboBox_progRefSerialPort.Items.Count > 0)
+                {
+                    int idx = comboBox_progRefSerialPort.Items.IndexOf(Properties.Settings.Default.SerialPortDevice);
+                    if (idx >= 0)
+                        comboBox_progRefSerialPort.SelectedIndex = idx;
+                    else
+                        comboBox_progRefSerialPort.SelectedIndex = 0;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Some default settings may not have loaded properly.");
             }
 
             comboBox_EEG.Items.AddRange(DaqSystem.Local.Devices);
-            if (comboBox_EEG.Items.Count > 0)
+            try
             {
-                int idx = comboBox_EEG.Items.IndexOf(Properties.Settings.Default.EEGDevice);
-                if (idx >= 0)
-                    comboBox_EEG.SelectedIndex = idx;
-                else
-                    comboBox_EEG.SelectedIndex = 0;
+                if (comboBox_EEG.Items.Count > 0)
+                {
+                    int idx = comboBox_EEG.Items.IndexOf(Properties.Settings.Default.EEGDevice);
+                    if (idx >= 0)
+                        comboBox_EEG.SelectedIndex = idx;
+                    else
+                        comboBox_EEG.SelectedIndex = 0;
+                }
             }
+            catch
+            {
+                MessageBox.Show("Some default settings may not have loaded properly.");
+            }
+
             comboBox_impedanceDevice.Items.AddRange(DaqSystem.Local.Devices);
-            if (comboBox_impedanceDevice.Items.Count > 0)
+            try
             {
-                int idx = comboBox_impedanceDevice.Items.IndexOf(Properties.Settings.Default.ImpedanceDevice);
-                if (idx >= 0)
-                    comboBox_impedanceDevice.SelectedIndex = idx;
-                else
-                    comboBox_impedanceDevice.SelectedIndex = 0;
+                if (comboBox_impedanceDevice.Items.Count > 0)
+                {
+                    int idx = comboBox_impedanceDevice.Items.IndexOf(Properties.Settings.Default.ImpedanceDevice);
+                    if (idx >= 0)
+                        comboBox_impedanceDevice.SelectedIndex = idx;
+                    else
+                        comboBox_impedanceDevice.SelectedIndex = 0;
+                }
             }
+            catch
+            {
+                MessageBox.Show("Some default settings may not have loaded properly.");
+            }
+
             comboBox_singleChannelPlaybackDevice.Items.AddRange(DaqSystem.Local.Devices);
-            if (comboBox_singleChannelPlaybackDevice.Items.Count > 0)
+            try
             {
-                int idx = comboBox_singleChannelPlaybackDevice.Items.IndexOf(Properties.Settings.Default.SingleChannelPlaybackDevice);
-                if (idx >= 0)
-                    comboBox_singleChannelPlaybackDevice.SelectedIndex = idx;
-                else
-                    comboBox_singleChannelPlaybackDevice.SelectedIndex = 0;
+                if (comboBox_singleChannelPlaybackDevice.Items.Count > 0)
+                {
+                    int idx = comboBox_singleChannelPlaybackDevice.Items.IndexOf(Properties.Settings.Default.SingleChannelPlaybackDevice);
+                    if (idx >= 0)
+                        comboBox_singleChannelPlaybackDevice.SelectedIndex = idx;
+                    else
+                        comboBox_singleChannelPlaybackDevice.SelectedIndex = 0;
+                }
             }
-            comboBox_IVControlDevice.Items.AddRange(DaqSystem.Local.Devices);
-            if (comboBox_IVControlDevice.Items.Count > 0)
+            catch
             {
-                int idx = comboBox_IVControlDevice.Items.IndexOf(Properties.Settings.Default.StimIvsVDevice);
-                if (idx >= 0)
-                    comboBox_IVControlDevice.SelectedIndex = idx;
-                else
-                    comboBox_IVControlDevice.SelectedIndex = 0;
+                MessageBox.Show("Some default settings may not have loaded properly.");
+            }
+
+            comboBox_IVControlDevice.Items.AddRange(DaqSystem.Local.Devices);
+            try
+            {
+                if (comboBox_IVControlDevice.Items.Count > 0)
+                {
+                    int idx = comboBox_IVControlDevice.Items.IndexOf(Properties.Settings.Default.StimIvsVDevice);
+                    if (idx >= 0)
+                        comboBox_IVControlDevice.SelectedIndex = idx;
+                    else
+                        comboBox_IVControlDevice.SelectedIndex = 0;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Some default settings may not have loaded properly.");
             }
 
             comboBox_SigOutDev.Items.AddRange(DaqSystem.Local.Devices);
-            if (comboBox_SigOutDev.Items.Count > 0)
+            try
             {
-                int idx = comboBox_SigOutDev.Items.IndexOf(Properties.Settings.Default.SigOutDev);
-                if (idx >= 0)
-                    comboBox_SigOutDev.SelectedIndex = idx;
-                else
-                    comboBox_SigOutDev.SelectedIndex = 0;
+                if (comboBox_SigOutDev.Items.Count > 0)
+                {
+                    int idx = comboBox_SigOutDev.Items.IndexOf(Properties.Settings.Default.SigOutDev);
+                    if (idx >= 0)
+                        comboBox_SigOutDev.SelectedIndex = idx;
+                    else
+                        comboBox_SigOutDev.SelectedIndex = 0;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Some default settings may not have loaded properly.");
             }
 
-            comboBox_AuxAnalogInputDevice.Items.AddRange
-                (DaqSystem.Local.Devices);
-            if (comboBox_AuxAnalogInputDevice.Items.Count > 0)
+            comboBox_AuxAnalogInputDevice.Items.AddRange(DaqSystem.Local.Devices);
+            try
             {
-                int idx = comboBox_AuxAnalogInputDevice.Items.IndexOf(Properties.Settings.Default.auxAnalogInDev);
-                if (idx >= 0)
-                    comboBox_AuxAnalogInputDevice.SelectedIndex = idx;
-                else
-                    comboBox_AuxAnalogInputDevice.SelectedIndex = 0;
+                if (comboBox_AuxAnalogInputDevice.Items.Count > 0)
+                {
+                    int idx = comboBox_AuxAnalogInputDevice.Items.IndexOf(Properties.Settings.Default.auxAnalogInDev);
+                    if (idx >= 0)
+                        comboBox_AuxAnalogInputDevice.SelectedIndex = idx;
+                    else
+                        comboBox_AuxAnalogInputDevice.SelectedIndex = 0;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Some default settings may not have loaded properly.");
             }
 
             listBox_AuxAnalogInChan.Items.AddRange
                 (DaqSystem.Local.GetPhysicalChannels(PhysicalChannelTypes.AI, PhysicalChannelAccess.External));
-            for (int i = listBox_AuxAnalogInChan.Items.Count - 1; i >= 0; --i)
+            try
             {
-                string tempChan = listBox_AuxAnalogInChan.Items[i].ToString();
-                string[] whatDev = tempChan.Split('/');
-                if (!((string)whatDev[0] == comboBox_AuxAnalogInputDevice.SelectedItem.ToString()))
-                    listBox_AuxAnalogInChan.Items.RemoveAt(i);
+                for (int i = listBox_AuxAnalogInChan.Items.Count - 1; i >= 0; --i)
+                {
+                    string tempChan = listBox_AuxAnalogInChan.Items[i].ToString();
+                    string[] whatDev = tempChan.Split('/');
+                    if (!((string)whatDev[0] == comboBox_AuxAnalogInputDevice.SelectedItem.ToString()))
+                        listBox_AuxAnalogInChan.Items.RemoveAt(i);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Some default settings may not have loaded properly.");
             }
 
             if (Properties.Settings.Default.auxAnalogInChan != null)
             {
-                for (int i = 0; i < Properties.Settings.Default.auxAnalogInChan.Count; ++i)
+                try
                 {
-                    int idx = listBox_AuxAnalogInChan.Items.IndexOf(Properties.Settings.Default.auxAnalogInChan[i]);
-                    if (idx >= 0)
-                        listBox_AuxAnalogInChan.SetSelected(idx, true);
-                    else
-                        listBox_AuxAnalogInChan.SetSelected(0, true);
+                    for (int i = 0; i < Properties.Settings.Default.auxAnalogInChan.Count; ++i)
+                    {
+                        int idx = listBox_AuxAnalogInChan.Items.IndexOf(Properties.Settings.Default.auxAnalogInChan[i]);
+                        if (idx >= 0)
+                            listBox_AuxAnalogInChan.SetSelected(idx, true);
+                        else
+                            listBox_AuxAnalogInChan.SetSelected(0, true);
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Some default settings may not have loaded properly.");
                 }
             }
 
@@ -322,8 +442,8 @@ namespace NeuroRighter
             if ((checkBox_EnableImpedanceMeasurements.Checked &&
                 comboBox_analogInputDevice1.SelectedItem.ToString() == comboBox_impedanceDevice.SelectedItem.ToString())
                 ||
-                (checkBox_EnableImpedanceMeasurements.Checked && 
-                checkBox_useSecondBoard.Checked && 
+                (checkBox_EnableImpedanceMeasurements.Checked &&
+                checkBox_useSecondBoard.Checked &&
                 comboBox_analogInputDevice2.SelectedItem.ToString() == comboBox_impedanceDevice.SelectedItem.ToString()))
             {
                 MessageBox.Show("Warning: the device selected for impedance measurements may cause conflicts at the hardware level because it is on the same board as electrode input.");
@@ -363,7 +483,7 @@ namespace NeuroRighter
             Properties.Settings.Default.ProcessMUA = checkBox_processMUA.Checked;
             Properties.Settings.Default.stimRobust = robustStim_checkbox.Checked;
             Properties.Settings.Default.useImpedanceMeasurer = checkBox_EnableImpedanceMeasurements.Checked;
-            Properties.Settings.Default.UseFloatingRef= checkBox_UseFloatingRef.Checked;
+            Properties.Settings.Default.UseFloatingRef = checkBox_UseFloatingRef.Checked;
 
             // Set up devices
             if (checkBox_useSecondBoard.Checked)
